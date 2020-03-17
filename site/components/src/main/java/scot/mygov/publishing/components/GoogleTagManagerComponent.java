44b8483cfa1914faac41da409fd19aa4feadcf92
David Sinclair <david.sinclair@gov.scot>
2020-03-17 12:35:15 +0000

    wip

--- site/components/src/main/java/scot/mygov/publishing/components/GoogleTagManagerComponent.java
+++ site/components/src/main/java/scot/mygov/publishing/components/GoogleTagManagerComponent.java
@@ -1,17 +1,34 @@
 package scot.mygov.publishing.components;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
 import org.hippoecm.hst.configuration.components.HstComponentConfiguration;
+import org.hippoecm.hst.configuration.hosting.Mount;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scot.mygov.publishing.HippoUtils;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import static org.apache.commons.lang3.StringUtils.*;
+
+/**
+ * Set fields required to correctly output google tag manager fields.
+ *
+ * We set the following attributes:
+ * - useLiveAnalytics: indicate to the template whether to use the values for gtmAuth and gtmEnv (this is set to true
+ *                     if both gtmAuth and gtmEnv have no empty values
+ * - gtmName: the format of the page, e.g. article
+ * - gtmId: slug for this page
+ * - gtmContainerId: the google tag manager container id
+ * - gtmAuth: value to use for the gtm_auth parameter
+ * - gtmEnv: value to use in the gtm_preview parameter
+ */
 public class GoogleTagManagerComponent extends BaseHstComponent {
 
     private static final Logger LOG = LoggerFactory.getLogger(GoogleTagManagerComponent.class);
@@ -22,13 +39,13 @@ public class GoogleTagManagerComponent extends BaseHstComponent {
 
         setGtmName(request);
         setGtmId(request);
-        setGtmAuthAndEnv(request);
+        setMountDependentAttributes(request);
     }
 
     /**
      * set gtmName based on the page component from the resolved sitemap item
      *
-     * the gtm name is the format fo ther page, e.g. 'article'
+     * the gtm name is the format of the page, e.g. 'article'
      */
     void setGtmName(HstRequest request) {
         HstComponentConfiguration componentConfig = request
@@ -52,32 +69,69 @@ public class GoogleTagManagerComponent extends BaseHstComponent {
     }
 
     /**
-     * gtmAuth and gtmEnv are set based on the path stored on the mount.  This path is used to look up a node
-     * containing the relevant values.
+     * gtmContainerId, gtmAuth and gtmEnv are set based on the path stored on the mount.
+     *
+     * This path is used to look up a node containing the relevant values.
      */
-    void setGtmAuthAndEnv(HstRequest request) {
+    void setMountDependentAttributes(HstRequest request) {
 
-        String gtmPath = request
+        Mount mount = request
                 .getRequestContext()
                 .getResolvedSiteMapItem()
                 .getResolvedMount()
-                .getMount()
-                .getProperty("publishing:gtm");
+                .getMount();
+        String gtmPath = mount.getProperty("publishing:gtm");
 
-        String auth = "";
-        String env = "";
-
-        try {
-            HstRequestContext requestContext = request.getRequestContext();
-            Session session = requestContext.getSession();
-            Node gtmNode = session.getNode(gtmPath);
-            auth = gtmNode.getProperty("publishing:auth").getString();
-            env = gtmNode.getProperty("publishing:env").getString();
-        } catch (RepositoryException e) {
-            LOG.error("Unable to get gtm value from mount: gtmPath is {}", gtmPath, e);
+        if (isBlank(gtmPath)) {
+            LOG.error("Mount has not publishing:gtm path:", mount.getName());
+            setEmptyGtmValues(request);
+            return;
         }
 
+        try {
+            Node gtmNode = getGtmNode(request, gtmPath);
+            if (gtmNode == null) {
+                setEmptyGtmValues(request);
+                return;
+            }
+
+            setGtmValues(request,
+                    gtmNode.getProperty("publishing:containerId").getString(),
+                    gtmNode.getProperty("publishing:auth").getString(),
+                    gtmNode.getProperty("publishing:env").getString());
+        } catch (RepositoryException e) {
+            LOG.error("Unexpected repository excpetion trying to set gtm values, gtmPath is {}", gtmPath, e);
+            setEmptyGtmValues(request);
+        }
+    }
+
+    void setGtmValues(HstRequest request, String containerId, String auth, String env) {
         request.setAttribute("gtmAuth", auth);
         request.setAttribute("gtmEnv", env);
+        request.setAttribute("gtmContainerId", containerId);
+        request.setAttribute("useLiveAnalytics", StringUtils.isNoneBlank(auth, env));
     }
+
+    void setEmptyGtmValues(HstRequest request) {
+        setGtmValues(request, "", "", "");
+    }
+
+    Node getGtmNode(HstRequest request, String path) throws RepositoryException {
+        HstRequestContext requestContext = request.getRequestContext();
+        Session session = requestContext.getSession();
+
+        if (!session.nodeExists(path)) {
+            LOG.error("gtm path does not exist: {}", path);
+            return null;
+        }
+
+        Node gtmHandle = session.getNode(path);
+        Node gtmNode = HippoUtils.getPublishedVariant(gtmHandle);
+        if (gtmNode == null) {
+            LOG.info("No published gtm document for path: {}", path);
+        }
+
+        return gtmNode;
+    }
+
 }
--- site/components/src/test/java/scot/mygov/publishing/components/GoogleTagManagerComponentTest.java
+++ site/components/src/test/java/scot/mygov/publishing/components/GoogleTagManagerComponentTest.java
@@ -8,12 +8,17 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.hippoecm.hst.core.request.ResolvedMount;
 import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
+import org.hippoecm.repository.HippoStdNodeType;
 import org.junit.Test;
+import scot.mygov.publishing.HippoUtils;
+import scot.mygov.publishing.TestUtil;
 
 import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+
+import java.util.Collections;
 
 import static org.mockito.Mockito.*;
 
@@ -27,7 +32,7 @@ public class GoogleTagManagerComponentTest {
 
         // ARRANGE
         GoogleTagManagerComponent sut = new GoogleTagManagerComponent();
-        HstRequest request = request("expectedGtmName", "expectedGtmId", "expectedGtmAuth", "expectedGtmEnv");
+        HstRequest request = request("expectedGtmName", "expectedGtmId", "expectedContainerId", "expectedGtmAuth", "expectedGtmEnv");
         HstResponse response = mock(HstResponse.class);
 
         // ACT
@@ -36,6 +41,7 @@ public class GoogleTagManagerComponentTest {
         // ASSERT
         verify(request).setAttribute("gtmName", "expectedGtmName");
         verify(request).setAttribute("gtmId", "expectedGtmId");
+        verify(request).setAttribute("gtmContainerId", "expectedContainerId");
         verify(request).setAttribute("gtmAuth", "expectedGtmAuth");
         verify(request).setAttribute("gtmEnv", "expectedGtmEnv");
     }
@@ -52,12 +58,48 @@ public class GoogleTagManagerComponentTest {
         sut.doBeforeRender(request, response);
 
         // ASSERT
+        verify(request).setAttribute("gtmContainerId", "");
         verify(request).setAttribute("gtmAuth", "");
         verify(request).setAttribute("gtmEnv", "");
     }
 
-    HstRequest request(String configName, String pathInfo, String auth, String env) throws RepositoryException {
-        Session session = sessionWithValues(auth, env);
+    @Test
+    public void noGtmPathOnMountResultsInEmptyValues() throws RepositoryException {
+
+        // ARRANGE
+        GoogleTagManagerComponent sut = new GoogleTagManagerComponent();
+        HstRequest request = requestWithGtmPath(null);
+        HstResponse response = mock(HstResponse.class);
+
+        // ACT
+        sut.doBeforeRender(request, response);
+
+        // ASSERT
+        verify(request).setAttribute("gtmContainerId", "");
+        verify(request).setAttribute("gtmAuth", "");
+        verify(request).setAttribute("gtmEnv", "");
+    }
+
+    @Test
+    public void gtmPathPointingAtNonExistantPathResultsInEmptyValues() throws RepositoryException {
+
+        // ARRANGE
+        GoogleTagManagerComponent sut = new GoogleTagManagerComponent();
+        Session session = mock(Session.class);
+        HstRequest request = request("", "", session);
+        HstResponse response = mock(HstResponse.class);
+
+        // ACT
+        sut.doBeforeRender(request, response);
+
+        // ASSERT
+        verify(request).setAttribute("gtmContainerId", "");
+        verify(request).setAttribute("gtmAuth", "");
+        verify(request).setAttribute("gtmEnv", "");
+    }
+
+    HstRequest request(String configName, String pathInfo, String containerId, String auth, String env) throws RepositoryException {
+        Session session = sessionWithValues(containerId, auth, env);
         return request(configName, pathInfo, session);
     }
 
@@ -86,18 +128,50 @@ public class GoogleTagManagerComponentTest {
         return request;
     }
 
+    HstRequest requestWithGtmPath(String gtmPath) throws RepositoryException {
+        HstRequest request = mock(HstRequest.class);
+
+        HstRequestContext context = mock(HstRequestContext.class);
+        when(request.getRequestContext()).thenReturn(context);
+
+        ResolvedSiteMapItem resolvedSiteMapItem = mock(ResolvedSiteMapItem.class);
+        when(context.getResolvedSiteMapItem()).thenReturn(resolvedSiteMapItem);
+        when(resolvedSiteMapItem.getPathInfo()).thenReturn("pathInfo");
+
+        HstComponentConfiguration hstComponentConfiguration = mock(HstComponentConfiguration.class);
+        when(resolvedSiteMapItem.getHstComponentConfiguration()).thenReturn(hstComponentConfiguration);
+        when(hstComponentConfiguration.getName()).thenReturn("configName");
+
+        ResolvedMount resolvedMount = mock(ResolvedMount.class);
+        when(resolvedSiteMapItem.getResolvedMount()).thenReturn(resolvedMount);
+
+        Mount mount = mock(Mount.class);
+        when(resolvedMount.getMount()).thenReturn(mount);
+        when(mount.getProperty("publishing:gtm")).thenReturn(gtmPath);
+
+        return request;
+    }
+
     HstRequest exceptionThrowingRequest(String configName, String pathInfo) throws RepositoryException {
         Session session = mock(Session.class);
         when(session.getNode(any())).thenThrow(new RepositoryException(""));
         return request(configName, pathInfo, session);
     }
 
-    Session sessionWithValues(String auth, String env) throws RepositoryException {
+    Session sessionWithValues(String containerId, String auth, String env) throws RepositoryException {
         Session session = mock(Session.class);
+        Node gtmHandle = mock(Node.class);
+        when(gtmHandle.getName()).thenReturn("handleName");
         Node gtmNode = mock(Node.class);
-        when(session.getNode("gtmPath")).thenReturn(gtmNode);
+        when(gtmHandle.getNodes("handleName")).thenReturn(TestUtil.iterator(Collections.singleton(gtmNode)));
+        when(session.nodeExists("gtmPath")).thenReturn(true);
+        when(session.getNode("gtmPath")).thenReturn(gtmHandle);
+        Property state = prop("published");
+        Property containerIdProp = prop(containerId);
         Property authProp = prop(auth);
         Property envProp = prop(env);
+        when(gtmNode.getProperty(HippoStdNodeType.HIPPOSTD_STATE)).thenReturn(state);
+        when(gtmNode.getProperty("publishing:containerId")).thenReturn(containerIdProp);
         when(gtmNode.getProperty("publishing:auth")).thenReturn(authProp);
         when(gtmNode.getProperty("publishing:env")).thenReturn(envProp);
         return session;
